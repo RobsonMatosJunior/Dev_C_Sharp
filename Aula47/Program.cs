fe6ab6f5574ffa3b8b368e85c095134e4a3479fd
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-04-22 15:31:51 -0300

    Finish aula 47

--- Aula47/Program.cs
+++ Aula47/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class program
+{
+    static void Main(string [] args)
+    {
+        
+        // Problema 1078
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i + " x " + n + " = " + i * n);
+        }
+    }
+}
