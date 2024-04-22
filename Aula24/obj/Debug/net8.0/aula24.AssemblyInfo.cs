96b63035bfd6767b5c1159a4fb185b66e5cf5588
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-04-22 15:02:28 -0300

    Add

--- Aula24/obj/Debug/net8.0/aula24.AssemblyInfo.cs
+++ Aula24/obj/Debug/net8.0/aula24.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula24")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24b324bf90fcd71c8e77290f9bc55cf9bbccf6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39134c0ac61b8f2ee90149b462e925cb9f14303a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula24")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula24")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
