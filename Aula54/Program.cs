86984e293821a41f6babc874716541923c49a7bb
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-13 17:24:35 -0300

    Aula vetores

--- Aula54/Program.cs
+++ Aula54/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] A;
+        double [] B;
+        string [] C;
+
+        A = new int[10];
+        B = new double[5];
+        C = new string[3];
+
+        A[3] = 10;
+        for (int i = 0; i<5; i++)
+        {
+            B[i] = i + 10;
+        }
+    }
+}
