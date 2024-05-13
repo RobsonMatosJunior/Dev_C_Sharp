aaff1227d15ec15cc1f7e9c2e48eb777f4a10025
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-13 17:10:16 -0300

    finish aula51

--- Aula51/Program.cs
+++ Aula51/Program.cs
@@ -3,6 +3,24 @@ class program
 {
     static void Main(string [] args)
     {
+        double C;
+        double F;
+        char repetir;
+
+        do
+        {
+            Console.WriteLine("DIgite a Temperatura em Celsius: ");
+            C= double.Parse(Console.ReadLine());
+            F = 9.0 * C / 5.0 + 32.0;
+            Console.WriteLine("Equivalente em fairheint é: " + F.ToString("F1"));
+
+            Console.WriteLine("Deseja repetir (s/n)?");
+            repetir = char.Parse(Console.ReadLine());
+        }
+
+        while (repetir == 's');
+        Console.ReadLine();
         
+
     }
 }
