b6b18d645e0f7428c96ab025fe5aee7af099f153
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-13 21:41:13 -0300

    meu Api consulta de placas

---
+++ testapicep/ConsumoApi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class ApiController
+{
+    private readonly HttpClient _client;
+
+    public ApiController()
+    {
+        _client = new HttpClient();
+        _client.BaseAddress = new Uri("https://viacep.com.br/");
+    }
+    public async Task<string> GetCEPAsync(string cep)
+    {
+        string url = $"ws/{cep}/json/";
+        HttpResponseMessage response = await _client.GetAsync(url);
+
+        if(response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            return "Erro ao consultar o CEP.";
+        }
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        ApiController apiController = new ApiController();
+        string cep = string.Empty;
+        char repetir = 'n';
+        string result = string.Empty;
+
+        do
+        {
+            Console.WriteLine("Digite o CEP: ");
+            cep = Console.ReadLine();
+            result = await apiController.GetCEPAsync(cep);
+            Console.WriteLine(result);
+
+            Console.WriteLine("Deseja repetir (s/n)?");
+            repetir = char.Parse(Console.ReadLine());
+        }
+
+        while (repetir == 's');
+        Console.ReadLine();
+        
+    }
+}
