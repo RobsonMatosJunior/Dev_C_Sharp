aa0c2f57a2218cbdd1a18a51234fcc6ca419de27
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-16 09:59:38 -0300

    inicio da Api com Refit

---
+++ ApiGBM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ApiGBM
+{
+    public interface IwaginsApi
+    {
+        [Get("/api/GetAll")]
+        Task<List<Wagon>> GetWagonsAsync();   
+    }
+
+    public class Wagon
+    {
+        public int Id {get; set; }
+        public string Plate  {get; set; }
+        public string Railroad  {get; set; }
+        public string Product  {get; set; }
+        public DateTime DownloadStartTime  {get; set; }
+        public DateTime DownloadEndTime  {get; set; }
+        public string Observation  {get; set; }
+        public int weight {get; set; }
+        public DateTime CreatedAt  {get; set; }
+        public DateTime UpdatedAt  {get; set; }
+        public object DeletedAt  {get; set; }       
+
+    }
+}
+
