41b71b3ab98a54439fdb86d9eb59d54662ff25e3
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-23 09:39:22 -0300

    Remover pasta obj do cache do Git

--- Aula46/obj/Debug/net8.0/Aula46.AssemblyInfo.cs
+++ Aula46/obj/Debug/net8.0/Aula46.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula46")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6cb4621086669a74356b953c8fdbe6291c2e7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2a2e442188e864f490e6350666947a899cd10ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula46")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula46")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
