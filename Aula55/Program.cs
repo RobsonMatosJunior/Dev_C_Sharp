45498b33f3fe5e162d7e9d61cc86f13cd9181c07
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-23 09:15:37 -0300

    aula 55

--- Aula55/Program.cs
+++ Aula55/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+    }
+}
