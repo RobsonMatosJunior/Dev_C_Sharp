fe5909f92652e44ebf18846f65c6c371c0237177
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-05-23 11:09:21 -0300

    aula 55 mid)

--- Aula55/Program.cs
+++ Aula55/Program.cs
@@ -3,6 +3,20 @@ class program
 {
     static void Main(string[] args)
     {
+
+        int N;
+        int[] vet;
+
+        N = int.Parse(Console.ReadLine());
+        vet = new int[N];
+
+        string[] s = Console.ReadLine().Split(" ");
+        for (int i=0; i<N; i++)
+        {
+            vet[i] = int.Parse(s[i]);
+        }
+        
+        
         Console.ReadLine();
     }
 }
