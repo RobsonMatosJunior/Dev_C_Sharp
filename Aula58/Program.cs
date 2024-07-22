77df67cf0700297c3d39bbf7dc086e1714c981c4
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-07-22 16:28:38 -0300

    Aula 58

--- Aula58/Program.cs
+++ Aula58/Program.cs
@@ -4,21 +4,43 @@ class program
     static void Main (string [] args)
     {
         int N;
-        double soma;
         double[] vet;
 
+        // Lê a quantidade de valores
         N = int.Parse(Console.ReadLine());
         vet = new double[N];
 
-        string[] s = Console.ReadLine().Split("");
+        // Lê os valores e os armazena no vetor
+        string[] s = Console.ReadLine().Split(' ');
 
-        soma = 0;
-        for (int i=0; i<N; i++)
+        for (int i = 0; i < N; i++)
         {
             vet[i] = double.Parse(s[i]);
         }
 
-        Console.WriteLine(s);
+        // Exibe os valores do vetor
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write(vet[i].ToString("F1") + " ");
+        }
+        
+        Console.WriteLine();
+
+        // Calcula a soma dos valores
+        double soma = 0;
+        for (int i = 0; i < N; i++)
+        {
+            soma += vet[i];
+        }
+        
+        // Calcula a média dos valores
+        double media = soma / N;
+
+        // Exibe a soma e a média dos valores
+        Console.WriteLine(soma.ToString("F2"));
+        Console.WriteLine(media.ToString("F2"));
+
         Console.ReadLine();
+
     }
 }
