92d8ed1e1f932f3086563753cc2778beeb49c34a
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-07-24 15:45:21 -0300

    Aula 60

--- Aula60/Program.cs
+++ Aula60/Program.cs
@@ -3,6 +3,21 @@ class program
 {
     static void Main (string [] args)
     {
+        int N;
+        N = int.Parse(Console.ReadLine());
 
+        string[] vet = new string[N];
+        
+        for (int i = 0; i<N; i++)
+        {
+            vet[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Nomes lidos: ");
+        foreach (string value in vet)
+        {
+            Console.WriteLine(value);  
+        }
+        Console.ReadLine();
     }
 }
