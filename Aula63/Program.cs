d401c1ed8986f70d2161a0422fc8385c2c094390
Robson Junior <111658874+RobsonMatosJunior@users.noreply.github.com>
2024-07-25 16:42:05 -0300

    Aula63

--- Aula63/Program.cs
+++ Aula63/Program.cs
@@ -1,8 +1,45 @@
-﻿using System;
+﻿// Aula sobre Matrizes
+
+using System;
 class program
 {
     static void Main (string [] args)
     {
+        /*
+        double[,] A;
+        A = new double[3, 4];
+        A[1, 2] = 10;
+        */
+
+        int M, N;
+        int [,] A;
+
+        M = int.Parse(Console.ReadLine());
+        N = int.Parse(Console.ReadLine());
+        A = new int[M, N];
+
+        for (int i = 0; i< M; i++)
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            for (int j = 0; j < N; j++)
+            {
+                A[i, j] = int.Parse(s[j]);        
+            }
+        }
         
+        Console.WriteLine("Impressa a Matriz fica :");
+
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(A [i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+        Console.ReadLine();
+
+
     }
 }
