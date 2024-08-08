e5a2beacfa9fc4c965f7a4fddc5a614c4e522502
RobsonMatosJunior <robsonjr819@gmail.com>
2024-08-07 21:55:13 -0300

    ajuste de conteudo de aulas

--- Aula67/Program.cs
+++ Aula67/Program.cs
@@ -3,35 +3,36 @@ class program
 {
     static void Main (string [] args)
     {
-        int M, N;
+        int M, N; //M = número de linhas e N = número de colunas
         int [,] mat;
 
-        string[] s = Console.ReadLine()Split(' ');
-        M = int.Parse(s[0]);
-        N = int.Parse(s[1]);
+        string[] s = Console.ReadLine().Split(' ');
+        M = int.Parse(s[0]); // Reading the number of lines and columns
+        N = int.Parse(s[1]); // Reading the number of lines and columns
+        mat = new int[M, N]; // Creating a matrix with M lines and N columns
 
-        for (int i=0; i<M; i++)
+        for (int i=0; i<M; i++)     // Reading the matrix
         {
             s = Console.ReadLine().Split(' '); // Splitting the string into an array of strings
-            for (int j=0; j<N; j++)
+            for (int j=0; j<N; j++)    // Reading the matrix
             {
-                mat[i,j] = int.Parse(s[j]);
+                mat[i,j] = int.Parse(s[j]); // Converting the string to an integer
             }
         }
 
-        int [] vet = new int [M];
-        for(int i=0; i<M; i++)
+        int [] vet = new int [M]; // Creating a vector with M elements
+        for(int i=0; i<M; i++)  // Summing the elements of each line and storing in the vector
         {
-            int soma = 0;
-            for (int j=0; j<N; j++)
+            int soma = 0; // Variable to store the sum of the elements of the line
+            for (int j=0; j<N; j++) // Summing the elements of the line
             {
-                soma += mat[i,j];
+                soma += mat[i,j]; // Summing the elements of the line
             }
-            vet[i] = soma;
+            vet[i] = soma; // Storing the sum of the elements of the line in the vector
         }
-        for (int i=0; i<M; i++)
+        for (int i=0; i<M; i++) // Printing the vector
         {
-            Console.WriteLine(vet[i]);
+            Console.WriteLine(vet[i]); // Printing the vector
         }
         
         Console.ReadLine();
